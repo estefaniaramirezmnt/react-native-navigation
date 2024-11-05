8c7d2e22e3fe10f65662256de23f95bcb0ac9e17
Estefanía <estefaniaramirezmnt@gmail.com>
2024-11-05 14:34:50 +0100

    Added navigation to go from home to details and viceversa

--- navigation/screens/DetailsScreen.js
+++ navigation/screens/DetailsScreen.js
@@ -1,21 +1,21 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
-const DetailsScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Details Screen</Text>
-            <Button title="Go to Home" />
-        </View>
-    );
-}
+const DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    </View>
+  );
+};
 
 export default DetailsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,21 +1,24 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from "react-native";
 
-const HomeScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>You're welcome</Text>
-            <Button title="Go to Details" />
-        </View>
-    );
-}
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>You're welcome</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate("Details")}
+      />
+    </View>
+  );
+};
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});    
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
